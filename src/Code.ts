d696e532dfb6f578d4dc51222fd998c2f0d4e343
Kevin_Griffin <kevin.griffin@gmail.com>
2023-12-21 10:27:25 -0500

    initial commit

---
+++ src/Code.ts
@@ -0,0 +1,8 @@
+/**
+ * https://developers.google.com/apps-script/guides/clasp
+ * https://developers.google.com/apps-script/guides/typescript
+ * https://github.com/google/clasp/blob/master/docs/typescript.md
+ * https://www.typescriptlang.org/docs/handbook/tsconfig-json.html#using-tsconfigjson-or-jsconfigjson
+ */
+
+function myFunction() {}
